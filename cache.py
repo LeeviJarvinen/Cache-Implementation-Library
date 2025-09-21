cd3c85a22d3875db24f016280564bf17d132108a
Leevi Järvinen <leevi2014@hotmail.com>
2025-09-21 19:55:42 +0300

    implemented TTLCache functionality

--- cache.py
+++ cache.py
@@ -1,3 +1,5 @@
+import time
+
 class DoublyLinkedList:
     class _Node:
         def __init__(self, key=None):
@@ -20,7 +22,7 @@ class DoublyLinkedList:
 
         return node
 
-    def remove_node(self, node):
+    def _remove_node(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
@@ -37,9 +39,14 @@ class DoublyLinkedList:
 
         last_node = self.tail.prev
         last_node_key = self.tail.prev.key
-        self.remove_node(last_node)
+        self._remove_node(last_node)
 
         return last_node_key
+
+    def remove_node(self, node):
+        if self.head.next == self.tail:
+            return None
+        self._remove_node(node)
 
 class BaseCache:
     def __init__(self, max_size: int):
@@ -53,7 +60,6 @@ class BaseCache:
         self.cache = {}
         self._setup_strategy()
 
-
     def get(self, key):
         if key in self.cache:
             self._on_access(key)
@@ -62,16 +68,16 @@ class BaseCache:
         return None
 
 
-    def set(self, key, value, ttl=None):
-        if isinstance(key, None)
+    def set(self, key, value):
+        if key == None:
             raise TypeError("key cannot be None")
-
+        
         if len(self.cache) > self.max_size and key not in self.cache:
             key_to_evict = self._get_eviction_key()
             del self.cache[key_to_evict]
 
         if key not in self.cache:
-            self._on_new_key(key, ttl)
+            self._on_new_key(key)
 
         if key in self.cache:
             self._on_access(key)
@@ -80,7 +86,7 @@ class BaseCache:
 
     def _setup_strategy(self): pass
     def _on_access(self, key): pass
-    def _on_new_key(self, key, ttl=None): pass
+    def _on_new_key(self, key): pass
     def _get_eviction_key(self): pass
     #def _on_eviction(self, key): pass
 
@@ -93,7 +99,7 @@ class LRUCache(BaseCache):
     def _on_access(self, key):
         self.dll.move_to_front(self.nodemap[key])
 
-    def _on_new_key(self, key, ttl = None):
+    def _on_new_key(self, key):
         node = self.dll.add_to_front(key)
         self.nodemap[key] = node
 
@@ -130,17 +136,56 @@ class FIFOCache(BaseCache):
     def _get_eviction_key(self):
         return self.insertion_order.pop(0)
 
-class TTLCache(BaseCache):
 
-    def _setup_strategy(self, default_ttl = None):
-        self.default_ttl = default_ttl
+class TTLCache:
+    def __init__(self, max_size: int, default_ttl = None):
+        if not isinstance(max_size, int):
+            raise TypeError("max_size must be an integer")
+
+        if max_size < 0:
+            raise ValueError("max_size cannot be negative")
+
+        self.dll = DoublyLinkedList()
+        self.max_size = max_size
+        self.cache = {}
         self.expiry_times = {}
+        self.nodemap = {}
+        self.default_ttl = default_ttl
 
-    def _on_access(self, key):
-        pass
+    def get(self, key):
+        if key in self.cache:
+            if time.time() > self.expiry_times[key]:
+                self.dll._remove_node(self.nodemap[key])
+                del self.nodemap[key]
+                del self.expiry_times[key]
+                del self.cache[key]
+                return None
+            return self.cache[key]
 
-    def _on_new_key(self, key, ttl):
-        pass
+        return None
 
-    def _get_eviction_key(self):
-        pass
+    def set(self, key, value, ttl = None):
+        if key == None:
+            raise TypeError("key cannot be None")
+
+        ttl = ttl or self.default_ttl
+
+        if ttl == None:
+            raise TypeError("Provide either an ttl value in set or a default ttl value for the object")
+        
+        if len(self.cache) > self.max_size and key not in self.cache:
+            last_key = self.dll.remove_last()
+            del self.expiry_times[last_key]
+            del self.cache[last_key]
+
+        if key not in self.cache:
+            node = self.dll.add_to_front(key)
+            self.nodemap[key] = node
+            self.expiry_times[key] = time.time() + ttl
+            self.cache[key] = value
+
+        if key in self.cache:
+            self.dll.move_to_front(self.nodemap[key])
+            self.expiry_times[key] = time.time() + ttl
+
+        self.cache[key] = value
